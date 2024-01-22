a40b286c03b4097ff4e7af0600ce1e825f16dc79
hyunho <mizzking75@gmail.com>
2024-01-22 23:42:25 +0900

    Initial Commit

---
+++ DisignPattern/DisignPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisignPatternApp.swift
+//  DisignPattern
+//
+//  Created by hyunho lee on 2024/01/22.
+//
+
+import SwiftUI
+
+@main
+struct DisignPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
