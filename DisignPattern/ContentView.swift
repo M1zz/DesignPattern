f4a90a019c2248dac50eb6233d96ca3e2695fbd1
hyunho <mizzking75@gmail.com>
2024-02-03 18:40:24 +0900

    first commit

--- DisignPattern/ContentView.swift
+++ DisignPattern/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            NameView()
+                .badge(2)
+                .tabItem {
+                    Label("Received", systemImage: "tray.and.arrow.down.fill")
+                }
+            MoneyView()
+                .tabItem {
+                    Label("Sent", systemImage: "tray.and.arrow.up.fill")
+                }
+            YoutubeView()
+                .badge("!")
+                .tabItem {
+                    Label("Account", systemImage: "person.crop.circle.fill")
+                }
         }
-        .padding()
+    }
+}
+
+struct NameView: View {
+    
+    @ObservedObject var userdata = UserManager.shared
+    
+    var body: some View {
+        VStack {
+            Text("안녕하세요 \(userdata.user.name)")
+            Button {
+                userdata.user.name = "리이오예"
+            } label: {
+                Text("터치")
+            }
+
+        }
+    }
+}
+
+struct MoneyView: View {
+    
+    @ObservedObject var userdata = UserManager.shared
+    
+    var body: some View {
+        Text("\(userdata.user.name)님의 잔액은 1000원 입니다")
+    }
+}
+
+struct YoutubeView: View {
+    
+    @ObservedObject var userdata = UserManager.shared
+    
+    var body: some View {
+        Text("\(userdata.user.name)님은 개발자리를 구독하고 있습니다")
     }
 }
 
--- DisignPattern/User.swift
+++ DisignPattern/User.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class UserManager: ObservableObject {
+    static let shared = UserManager()
+    
+    private init() {}
+    
+    @Published var user: User = User(name: "리이오", money: 0, youtube: "")
+}
+
+struct User {
+    var name: String
+    var money: Int
+    var youtube: String
+}
